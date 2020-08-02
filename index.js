d453aba6042fb1cdccda38406ef3d3fc99b732d0
Jamie Lopez <jmelopez@outlook.com>
2020-08-02 16:17:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ function whisper(string) {
    return string.toLowerCase()
 }
 function logShout(string){
-  console.log string.toUpperCase()
-}
+  if(string.toUpperCase){
+    console.log ("YES INDEED!")
+  }}
